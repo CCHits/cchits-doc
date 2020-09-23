aa620e55e88d5bd4816ad0e92cd37e1f012d26a3
Yannick Mauray <yannick.mauray@gmail.com>
2020-09-24 00:13:40 +0200

    Added 'about' page

--- conf.py
+++ conf.py
@@ -18,7 +18,7 @@
 # -- Project information -----------------------------------------------------
 
 project = 'CCHits'
-copyright = '2020, John Sprigss, Yannick Mauray, Dave Lee'
+copyright = '2020, John Spriggs, Yannick Mauray, Dave Lee (in not particular order)'
 author = 'CCHits.net collective'
 
 
@@ -28,7 +28,8 @@ author = 'CCHits.net collective'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-'recommonmark'
+    'recommonmark',
+    'sphinx_rtd_theme'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -45,7 +46,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'default'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
